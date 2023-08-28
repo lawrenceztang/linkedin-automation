a009a440e1d09f503618b69968f66db3cd6c14c8
Lawrence Tang <30534591+lawrenceztang@users.noreply.github.com>
2023-08-27 23:41:00 -0700

    Working for Trist

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ import os
 import time
 import csv
 
-message = "I'd like to connect!"
+#message = "Hi there, I am an intern at HPE and third year undergrad at UChicago. I'm applying to Jane Street for the Quantitative Trader Internship, and I wanted to see if you were interested in referring as I know Jane Street is especially competitive. Let me know if you would like to chat first!"
+message = "Hi there, I'm applying to [company] for the Software Engineer role. Could you refer me?"
 companies_file = "companies_short.csv"
 day_limit = 50
-limit_per_company = 50
-password = "MY_PASSWORD"
-username = "MY_USERNAME"
-people_query = "?facetSchool=18314"
-webdriver_path = "C:/Users/larry/Downloads/chromedriver_win32/chromedriver.exe"
-
+limit_per_company = 15
 
 def get_companies_list():
     out = []
@@ -23,44 +19,17 @@ def get_companies_list():
             out.append(row[0])
     return out
 
-def scroll_down(driver):
-    SCROLL_PAUSE_TIME = 0.5
-
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-
-    while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-
 def replace_message(company, driver, name):
     new_message = message
     new_message = new_message.replace("[company]", company)
     new_message = new_message.replace("there", name)
     return new_message
 
-def login(driver):
-    driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
-    input = driver.find_element_by_xpath("//input[@id='username']")
-    input.send_keys(username)
-    input = driver.find_element_by_xpath("//input[@id='password']")
-    input.send_keys(password)
-    driver.find_element_by_xpath("//button[@class='btn__primary--large from__button--floating']").click()
-
 def main():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument("user-data-dir=%s" % os.path.join(os.path.dirname(os.path.realpath(__file__)), "selenium"))
-    driver = webdriver.Chrome(executable_path=webdriver_path, chrome_options=options)
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.linkedin.com")
 
     time.sleep(1)
@@ -73,7 +42,7 @@ def main():
         driver.get(f"https://www.linkedin.com/search/results/all/?keywords={company}&origin=GLOBAL_SEARCH_HEADER&sid=.wg)")
         time.sleep(4)
         try:
-            a = driver.find_element_by_xpath(
+            a = driver.find_element("xpath",
                 "//a[@class='app-aware-link  search-nec__hero-kcard-v2-link-wrapper link-without-hover-state link-without-visited-state t-normal t-black--light']").get_attribute(
                 "href")
             # a = "https://www.linkedin.com/company/d.-e.-shaw-&-co./"
@@ -87,10 +56,10 @@ def main():
 
         company_count = 0
         while company_count < limit_per_company and day_count < day_limit:
-            list = driver.find_elements_by_xpath("//span[text()='Connect']/..")
+            list = driver.find_elements("xpath", "//span[text()='Connect']/..")
             if len(list) == 0:
                 try:
-                    driver.find_element_by_xpath("//span[text()='Show more results']").click()
+                    driver.find_element("xpath", "//span[text()='Show more results']").click()
                 except:
                     pass
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -107,26 +76,26 @@ def main():
                         
                         continue
                     time.sleep(1)
-                    name = driver.find_element_by_xpath("//strong").text.split(" ")[0]
+                    name = driver.find_element("xpath", "//strong").text.split(" ")[0]
                     try:
-                        driver.find_element_by_xpath("//button[@aria-label='Add a note']").click()
+                        driver.find_element("xpath", "//button[@aria-label='Add a note']").click()
                     except:
                         driver.refresh()
                         continue
                     try:
-                        driver.find_element_by_xpath("//textarea").send_keys(replace_message(company, driver, name))
+                        driver.find_element("xpath", "//textarea").send_keys(replace_message(company, driver, name))
                     except:
-                        driver.find_element_by_xpath("//textarea").send_keys(replace_message(company, driver, "there"))
+                        driver.find_element("xpath", "//textarea").send_keys(replace_message(company, driver, "there"))
                     time.sleep(.1)
                     try:
-                        driver.find_element_by_xpath("//input[@type='email']").click()
+                        driver.find_element("xpath", "//input[@type='email']").click()
                         driver.refresh()
                         count_bad += 1
                         continue
                     except:
-                        driver.find_element_by_xpath("//button[@aria-label='Send now']").click()
+                        driver.find_element("xpath", "//button[@aria-label='Send now']").click()
                     try:
-                        driver.find_element_by_xpath("//svg[@xmlns='http://www.w3.org/2000/svg']").click()
+                        driver.find_element("xpath", "//svg[@xmlns='http://www.w3.org/2000/svg']").click()
                     except:
                         pass
                     time.sleep(random.random() + 1)
